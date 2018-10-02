e1af2872d2d13270704d370672e11b50cab4d290
Shreyansh Lodha <slodha96@gmail.com>
2018-10-03 03:07:30 +0530

    Add kotlin support and Convert Adapter and Models to Kotlin
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

---
+++ app/src/main/java/me/akhilnarang/eztvapp/Model/EZTVModel.kt
@@ -0,0 +1,21 @@
+package me.akhilnarang.eztvapp.Model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class EZTVModel {
+
+    @SerializedName("torrents_count")
+    @Expose
+    var torrentsCount: Int? = null
+    @SerializedName("limit")
+    @Expose
+    var limit: Int? = null
+    @SerializedName("page")
+    @Expose
+    var page: Int? = null
+    @SerializedName("torrents")
+    @Expose
+    var torrents: List<Torrent>? = null
+
+}
---
+++ app/src/main/java/me/akhilnarang/eztvapp/Model/Torrent.kt
@@ -0,0 +1,27 @@
+package me.akhilnarang.eztvapp.Model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+class Torrent : Serializable {
+    @SerializedName("filename")
+    @Expose
+    var filename: String? = null
+    @SerializedName("episode_url")
+    @Expose
+    var episodeUrl: String? = null
+    @SerializedName("torrent_url")
+    @Expose
+    var torrentUrl: String? = null
+    @SerializedName("magnet_url")
+    @Expose
+    var magnetUrl: String? = null
+    @SerializedName("small_screenshot")
+    @Expose
+    var smallScreenshot: String? = null
+    @SerializedName("large_screenshot")
+    @Expose
+    var largeScreenshot: String? = null
+
+}
